74eff2ab5fbd0b76fea42bc135b4f404c3216d35
Jon Betts <jon.betts@hypothes.is>
2021-02-09 15:02:06 +0000

    Remove the old URL token routines

--- src/h_vialib/secure/token.py
+++ src/h_vialib/secure/token.py
@@ -1,13 +1,10 @@
 """JWT based tokens which can be used to create verifiable, expiring tokens."""
 
-from urllib.parse import urlparse, urlunparse
-
 import jwt
 from jwt import DecodeError, ExpiredSignatureError, InvalidSignatureError
 
-from h_vialib import Configuration
 from h_vialib.exceptions import InvalidToken, MissingToken
-from h_vialib.secure.expiry import as_expires, quantized_expiry
+from h_vialib.secure.expiry import as_expires
 
 
 class SecureToken:
@@ -56,95 +53,3 @@ class SecureToken:
             raise InvalidToken("Expired secure token") from err
         except DecodeError as err:
             raise InvalidToken("Malformed secure token") from err
-
-
-class SecureURLToken(SecureToken):
-    """A secure token used for signing and checking URLs."""
-
-    def create(
-        self, url, payload, expires=None, max_age=None
-    ):  # pylint: disable=arguments-differ
-        """Create a secure token.
-
-        :param url: The URL to include in the signature
-        :param payload: Dict of information to put in the token
-        :param expires: Datetime by which this token with expire
-        :param max_age: ... or max age in seconds after which this will expire
-        :return: A JWT encoded token as a string
-
-        :raise ValueError: if neither expires nor max_age is specified, or no
-            URL is provided
-        """
-        if not url:
-            raise ValueError("A URL is required to create a token")
-
-        payload["url"] = self.normalize_url(url)
-
-        return super().create(payload, expires, max_age)
-
-    def verify(self, token, comparison_url):  # pylint: disable=arguments-differ
-        """Decode a token and check for validity and a matching URL.
-
-        :param token: Token string to check
-        :param comparison_url: URL to check against the contents of the token
-        :return: The token payload if valid
-        :raise InvalidToken: If the token is invalid or expired, or if
-            the URLs do not match
-
-        :raise MissingToken: If no token is provided
-        """
-        decoded = super().verify(token)
-
-        signed_url = decoded.get("url")
-        if not signed_url:
-            raise InvalidToken("Secure URL token contains no URL")
-
-        comparison_url = self.normalize_url(comparison_url)
-        if signed_url != comparison_url:
-            raise InvalidToken(
-                f"Secure URL token path mismatch: Got '{comparison_url}' expected '{signed_url}'"
-            )
-
-        return decoded
-
-    @classmethod
-    def normalize_url(cls, url):
-        """Normalize a URL for comparison."""
-
-        try:
-            parts = urlparse(url)
-        except ValueError:
-            # This URL is unparseable, so this is the best we can do
-            return url
-
-        # Ensure that http://example.com and http://example.com/ are considered
-        # the same
-        if not parts.path:
-            parts = parts._replace(path="/")
-
-        return urlunparse(parts)
-
-
-class ViaSecureURLToken(SecureURLToken):
-    """A token for signing proxied URLs."""
-
-    MAX_AGE = 60 * 60  # An hour
-
-    def create(self, proxied_url):  # pylint: disable=arguments-differ
-        """Create a secure token for a Via proxied URL.
-
-        :param proxied_url: The URL you intend to proxy.
-        :return: A JWT encoded token as a string
-        """
-        return super().create(
-            proxied_url, payload={}, expires=quantized_expiry(self.MAX_AGE)
-        )
-
-    @classmethod
-    def normalize_url(cls, url):
-        """Normalize a URL for comparison."""
-
-        # We strip any Via config off before comparison, to ensure config
-        # doesn't change the token, and to allow the token to be included in
-        # that config
-        return Configuration.strip_from_url(super().normalize_url(url))
--- tests/unit/h_vialib/secure/token_test.py
+++ tests/unit/h_vialib/secure/token_test.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 
 import jwt
 import pytest
@@ -6,7 +6,7 @@ from h_matchers import Any
 from jwt import DecodeError, ExpiredSignatureError, InvalidSignatureError
 
 from h_vialib.exceptions import InvalidToken, MissingToken
-from h_vialib.secure.token import SecureToken, SecureURLToken, ViaSecureURLToken
+from h_vialib.secure.token import SecureToken
 
 
 def decode_token(token_string):
@@ -64,93 +64,3 @@ class TestSecureToken:
     @pytest.fixture
     def jwt(self, patch):
         return patch("h_vialib.secure.token.jwt")
-
-
-class TestSecureURLToken:
-    def test_create_normalizes_and_packs_the_url(self, token):
-        expires = datetime.now() + timedelta(seconds=10)
-
-        token_string = token.create("http://example.com", {"a": 2}, expires)
-
-        assert token_string == Any.string()
-        assert decode_token(token_string) == {
-            "url": "http://example.com/",
-            "a": 2,
-            "exp": Any.int(),
-        }
-
-    @pytest.mark.parametrize("url", (None, ""))
-    def test_create_requires_the_url(self, token, url):
-        with pytest.raises(ValueError):
-            token.create(url, {}, max_age=10)
-
-    def test_verify_works_with_a_matching_url(self, token):
-        token_string = token.create("http://example.com", {}, max_age=10)
-
-        decoded = token.verify(token_string, "http://example.com/")
-
-        assert decoded == Any.dict.containing({"url": "http://example.com/"})
-
-    def test_verify_fails_if_there_is_no_url_in_the_token(self, token):
-        no_url_token = SecureToken(token._secret).create({}, max_age=10)
-
-        with pytest.raises(InvalidToken):
-            token.verify(no_url_token, "http://any.example.com/")
-
-    def test_verify_fails_if_there_is_url_mismatch(self, token):
-        token_string = token.create("http://example.com", {}, max_age=10)
-
-        with pytest.raises(InvalidToken):
-            token.verify(token_string, "http://DIFFERENT.example.com/")
-
-    @pytest.mark.parametrize(
-        "url,normalized",
-        (
-            ("http://example.com/", "http://example.com/"),
-            ("http://example.com", "http://example.com/"),
-            ("http://example.com]", "http://example.com]"),
-        ),
-    )
-    @pytest.mark.parametrize("query_string", ("", "?a=b"))
-    def test_normalize_url(self, token, url, normalized, query_string):
-        result = token.normalize_url(url + query_string)
-
-        assert result == normalized + query_string
-
-    @pytest.fixture
-    def token(self):
-        return SecureURLToken("a_very_secret_secret")
-
-
-class TestViaSecureURLToken:
-    def test_create_works(self, token):
-        token_string = token.create("http://example.com?via.config=blah")
-
-        assert token_string == Any.string()
-        assert decode_token(token_string) == {
-            "url": "http://example.com/",
-            "exp": Any.int(),
-        }
-
-    def test_create_uses_a_quantized_expiry(self, token, quantized_expiry):
-        quantized_expiry.return_value = datetime.now(tz=timezone.utc)
-
-        token_string = token.create("*any*")
-
-        quantized_expiry.assert_called_once_with(token.MAX_AGE)
-        assert decode_token(token_string) == Any.dict.containing(
-            {"exp": int(quantized_expiry.return_value.timestamp())}
-        )
-
-    def test_normalize_url_strips_via_params(self, token):
-        url = token.normalize_url("http://example.com?a=b&via.config=boo")
-
-        assert url == "http://example.com/?a=b"
-
-    @pytest.fixture
-    def token(self):
-        return ViaSecureURLToken("a_very_secret_secret")
-
-    @pytest.fixture
-    def quantized_expiry(self, patch):
-        return patch("h_vialib.secure.token.quantized_expiry")
