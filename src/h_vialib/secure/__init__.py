d977e872c3444b7483e6f554f16d08aa8773879f
Jon Betts <jon.betts@hypothes.is>
2021-02-09 15:01:03 +0000

    Add new secure URL signing routines
    We really should have taken 5 minutes to discuss how we were going to use
    these, but in the end we've come down on the side of signing the entire
    URL. This meant the existing stuff wasn't actually very convenient for that.
    
    This packs all the goods into one routine.

--- src/h_vialib/secure/__init__.py
+++ src/h_vialib/secure/__init__.py
@@ -2,3 +2,4 @@
 
 from h_vialib.secure.expiry import quantized_expiry
 from h_vialib.secure.token import SecureToken, SecureURLToken, ViaSecureURLToken
+from h_vialib.secure.url import SecureURL, ViaSecureURL
