f022582161327005e464902d1aa496a600b532a7
Jon Betts <jon.betts@hypothes.is>
2022-05-10 18:14:50 +0100

    Allow "contentProvider" flag to be passed through
    This lets LMS set the content provider with config

--- src/h_vialib/_params.py
+++ src/h_vialib/_params.py
@@ -14,6 +14,7 @@ class Params:
         "openSidebar",
         "requestConfigFromFrame",
         # Things which seem safe
+        "contentPartner",
         "enableExperimentalNewNoteButton",
         "experimental",  # Nested value for experimental features
         "externalContainerSelector",
