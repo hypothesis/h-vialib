10640cce8c7814f58449ca7ac0a03ef97def2879
Marcos Prieto <marcospri@gmail.com>
2022-12-22 16:47:33 +0100

    Include hardcoded outputs on token tests
    This will make it easier to validate the change to another JWT library.

--- tests/unit/h_vialib/secure/token_test.py
+++ tests/unit/h_vialib/secure/token_test.py
@@ -8,12 +8,28 @@ from jwt import DecodeError, ExpiredSignatureError, InvalidSignatureError
 from h_vialib.exceptions import InvalidToken, MissingToken
 from h_vialib.secure.token import SecureToken
 
+from freezegun import freeze_time
+
+
 
 def decode_token(token_string):
     return jwt.decode(token_string, "a_very_secret_secret", SecureToken.TOKEN_ALGORITHM)
 
 
 class TestSecureToken:
+
+    @freeze_time("2022-12-22")
+    @pytest.mark.parametrize("payload,expires,output", [
+        ({"a": 2}, timedelta(seconds=10), "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhIjoyLCJleHAiOjE2NzE2NjcyMTB9.k5vQWx-k_u_xRDE2wgZhh7DfK75Wt5LnOwDB4tPBA_k"),
+        ({"a": 2}, timedelta(seconds=20), "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhIjoyLCJleHAiOjE2NzE2NjcyMjB9.SZWwElky3JdiR_Xpx1pmQ2k6Kzjnxgm0-KY6AL9Q_Ws"),
+        ({"b": 5}, timedelta(seconds=10), "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiIjo1LCJleHAiOjE2NzE2NjcyMTB9.Gf1e9jLnn57iGGU4c7nit6zIZ6LWNIQTHiOzk9cLJ9c"),
+    ])
+    def test_create_output(self, token, payload, expires, output):
+        token_string = token.create(payload, expires=datetime.now() + expires)
+
+        assert token_string == output
+
+
     def test_create_works(self, token):
         expires = datetime.now() + timedelta(seconds=10)
 
