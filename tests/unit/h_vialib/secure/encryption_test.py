744dbf0d77150044442dcc6ce3ae1df8ac1cffb5
Marcos Prieto <marcospri@gmail.com>
2022-12-22 18:14:10 +0100

    Switch over JWEs
    Instead of our own mix of base64,AES,iv... just use JWE for the same
    functionality.

--- tests/unit/h_vialib/secure/encryption_test.py
+++ tests/unit/h_vialib/secure/encryption_test.py
@@ -1,6 +1,5 @@
 import pytest
-from Cryptodome.Random import get_random_bytes
-from Cryptodome.Util.Padding import pad
+from jose import constants
 
 from h_vialib.secure import Encryption
 
@@ -13,58 +12,39 @@ class TestEncryption:
 
         assert encryption.decrypt_dict(encrypted) == payload_dict
 
-    def test_encrypt_dict(self, encryption, secret, AES, Random, base64, json):
+    def test_encrypt_dict(self, encryption, secret, json, jwe):
         payload_dict = {"some": "data"}
 
         encrypted = encryption.encrypt_dict(payload_dict)
 
-        Random.new.return_value.read.assert_called_once_with(AES.block_size)
-        AES.new.assert_called_once_with(
-            pad(secret, 16), AES.MODE_CFB, Random.new.return_value.read.return_value
+        json.dumps.assert_called_with(payload_dict)
+        jwe.encrypt.assert_called_once_with(
+            json.dumps.return_value,
+            secret.ljust(32),
+            algorithm=constants.ALGORITHMS.DIR,
+            encryption=constants.ALGORITHMS.A128CBC_HS256,
         )
+        assert encrypted == jwe.encrypt.return_value.decode.return_value
 
-        AES.new.return_value.encrypt.assert_called_once_with(
-            json.dumps.return_value.encode.return_value
-        )
-        json.dumps.assert_called_with(
-            {
-                "iv": base64.urlsafe_b64encode.return_value.decode.return_value,
-                "payload": base64.urlsafe_b64encode.return_value.decode.return_value,
-            }
-        )
-        assert encrypted == json.dumps.return_value
-
-    def test_decrypt_dict(self, encryption, secret, AES, base64, json):
+    def test_decrypt_dict(self, encryption, secret, jwe, json):
         plain_text_dict = encryption.decrypt_dict("payload")
 
-        AES.new.assert_called_once_with(
-            pad(secret, 16), AES.MODE_CFB, base64.urlsafe_b64decode.return_value
-        )
-        AES.new.return_value.decrypt.assert_called_once_with(
-            base64.urlsafe_b64decode.return_value
-        )
+        jwe.decrypt.assert_called_once_with("payload", secret.ljust(32))
+        json.loads.assert_called_once_with(jwe.decrypt.return_value)
         assert plain_text_dict == json.loads.return_value
 
     @pytest.fixture
     def secret(self):
-        return get_random_bytes(12)
+        return b"VERY SECRET"
 
     @pytest.fixture
     def encryption(self, secret):
         return Encryption(secret)
 
     @pytest.fixture
-    def AES(self, patch):
-        return patch("h_vialib.secure.encryption.AES")
-
-    @pytest.fixture
-    def Random(self, patch):
-        return patch("h_vialib.secure.encryption.Random")
-
-    @pytest.fixture
-    def base64(self, patch):
-        return patch("h_vialib.secure.encryption.base64")
-
-    @pytest.fixture
     def json(self, patch):
         return patch("h_vialib.secure.encryption.json")
+
+    @pytest.fixture
+    def jwe(self, patch):
+        return patch("h_vialib.secure.encryption.jwe")
