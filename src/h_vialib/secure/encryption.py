e0c203e869c6e5f003c1ec27457b5a8547940928
Marcos Prieto <marcospri@gmail.com>
2024-04-30 12:55:34 +0200

    Replace python-jose with joserfc
    Python jose has some maintainability issues. Replacing it with joserfc
    which powers the authlib library.
    
    - Replace python-jose for encryption
    - Replace python-jose for signed tokens

--- src/h_vialib/secure/encryption.py
+++ src/h_vialib/secure/encryption.py
@@ -1,26 +1,23 @@
 import json
 
-from jose import constants, jwe
+from joserfc import jwe
 
 
 class Encryption:
-    JWE_ALGORITHM = constants.ALGORITHMS.DIR
-    JWE_ENCRYPTION = constants.ALGORITHMS.A128CBC_HS256
+    JWE_ALGORITHM = "dir"
+    JWE_ENCRYPTION = "A128CBC-HS256"
 
     def __init__(self, secret: bytes):
         self._secret = secret.ljust(32)[:32]
 
     def encrypt_dict(self, payload: dict) -> str:
         """Encrypt a dictionary as a JWE."""
-
-        return jwe.encrypt(
-            json.dumps(payload),
-            self._secret,
-            algorithm=self.JWE_ALGORITHM,
-            encryption=self.JWE_ENCRYPTION,
-        ).decode("utf-8")
+        protected = {"alg": self.JWE_ALGORITHM, "enc": self.JWE_ENCRYPTION}
+        return jwe.encrypt_compact(
+            protected, json.dumps(payload).encode("utf-8"), self._secret
+        )
 
     def decrypt_dict(self, payload: str) -> dict:
         """Decrypts payloads created by `encrypt_dict`."""
-
-        return json.loads(jwe.decrypt(payload, self._secret))
+        data = jwe.decrypt_compact(payload, self._secret).plaintext
+        return json.loads(data)
--- tests/unit/h_vialib/secure/encryption_test.py
+++ tests/unit/h_vialib/secure/encryption_test.py
@@ -1,5 +1,4 @@
 import pytest
-from jose import constants
 
 from h_vialib.secure import Encryption
 
@@ -18,20 +17,28 @@ class TestEncryption:
         encrypted = encryption.encrypt_dict(payload_dict)
 
         json.dumps.assert_called_with(payload_dict)
-        jwe.encrypt.assert_called_once_with(
-            json.dumps.return_value,
+        jwe.encrypt_compact.assert_called_once_with(
+            {"alg": encryption.JWE_ALGORITHM, "enc": encryption.JWE_ENCRYPTION},
+            json.dumps.return_value.encode.return_value,
             secret.ljust(32),
-            algorithm=constants.ALGORITHMS.DIR,
-            encryption=constants.ALGORITHMS.A128CBC_HS256,
         )
-        assert encrypted == jwe.encrypt.return_value.decode.return_value
+        assert encrypted == jwe.encrypt_compact.return_value
 
-    def test_decrypt_dict(self, encryption, secret, jwe, json):
+    def test_decrypt_dict(self, encryption, secret, json, jwe):
         plain_text_dict = encryption.decrypt_dict("payload")
 
-        jwe.decrypt.assert_called_once_with("payload", secret.ljust(32))
-        json.loads.assert_called_once_with(jwe.decrypt.return_value)
+        jwe.decrypt_compact.assert_called_once_with("payload", secret.ljust(32))
+        json.loads.assert_called_once_with(jwe.decrypt_compact.return_value.plaintext)
         assert plain_text_dict == json.loads.return_value
+
+    def test_decrypt_dict_hardcoded(self, encryption):
+        # Copied from the output of decrypt_dict.
+        # Useful to check backwards compatibility when updating the crypto backend
+        encrypted = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..q7UXaHtenyFA5VD3QhrxXA.gkAmUrzmW5UFpuF_tZLmcUzUfS9FuLAiV_xqRJBVJ3Y.U42rUD65NVjH-SoFfeDoOw"
+
+        plain_text_dict = encryption.decrypt_dict(encrypted)
+
+        assert plain_text_dict == {"some": "data"}
 
     @pytest.fixture
     def secret(self):
